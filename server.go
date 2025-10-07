f8515d5b2b7ae6c82c9acab658ee60e38062d6c9
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-06 22:40:29 -0600

    created Meetup, Friend, and User structs

--- server.go
+++ server.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"hash"
 	"net/http"
+	"time"
 )
+
+type Meetup struct {
+	id                int
+	time              time.Time
+	place             string
+	friends_attending []Friend
+}
+
+type Friend struct {
+	id                          int
+	name                        string
+	birthday                    time.Time
+	days_since_last_interaction int
+	days_since_last_meetup      int
+	phone_number                string // To be encrypted
+	meetup_plans                []Meetup
+}
+
+type User struct {
+	id                                         int
+	username                                   string
+	password                                   hash.Hash64 // To be encrypted
+	days_since_you_last_interacted_with_anyone int
+	days_since_you_last_hung_out_with_anyone   int
+	meetup_plans                               []Meetup
+	recievesNotifications                      (map[string]bool)
+}
 
 func main() {
 	http.ListenAndServe(":3000", nil)
