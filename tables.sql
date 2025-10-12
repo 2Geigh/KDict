885508c11ee1d2499af0a18a3cb00bfc0470b995
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-12 15:36:21 -0600

    fixed typo in tables.sql

--- tables.sql
+++ tables.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY, -- serial means it increments upon instantiation, and primary key forces the value to be unique
     username VARCHAR(255) NOT NULL UNIQUE, -- not null means the value is required, and unique means it cant appear more than once
-    password BYTEA NOT NULL -- bytea is raw byte data
+    password BYTEA NOT NULL, -- bytea is raw byte data
     days_since_last_interaction INTEGER NOT NULL DEFAULT 0,
     days_since_last_meetup INTEGER DEFAULT 0,
     meetup_plans JSONB,
