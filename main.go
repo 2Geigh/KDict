5bfa9a3c498b50452f2acc497225dab954130d10
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-13 22:23:53 -0600

    parsed XML into dictSearch struct

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,14 +9,41 @@ import (
 	"os"
 	"os/exec"
 
-	// "encoding/xml"
+	// "html/template"
 
 	"github.com/joho/godotenv"
+)
+
+const (
+	API_URL = "https://krdict.korean.go.kr/api/search?key="
 )
 
 var (
 	API_BASE_URL string
 )
+
+type dictSearch struct {
+	Title   string     `xml:"title"`
+	Total   int        `xml:"total"`
+	Results []dictItem `xml:"item"`
+}
+
+type dictItem struct {
+	Target_code      int              `xml:"target_code"`
+	Word             string           `xml:"word"`
+	Sup_no           int              `xml:"sup_no"`
+	Etymology        string           `xml:"origin"`
+	Pronunciation    string           `xml:"pronunciation"`
+	Word_grade_level string           `xml:"word_grade"`
+	Word_type        string           `xml:"pos"`
+	Entry_link       string           `xml:"link"`
+	Sense            dict_entry_sense `xml:"sense"`
+}
+
+type dict_entry_sense struct {
+	Order      int    `xml:"sense_order"`
+	Definition string `xml:"definition"`
+}
 
 func search_word(word string) (*exec.Cmd, error) {
 	curl, err := exec.LookPath("curl")
@@ -31,11 +59,15 @@ func search_word(word string) (*exec.Cmd, error) {
 	return cmd, err
 }
 
+func parseXML(data any) {
+
+}
+
 func main() {
 
 	godotenv.Load()
 	API_KEY := os.Getenv("API_KEY")
-	API_BASE_URL = "https://krdict.korean.go.kr/api/search?key=" + API_KEY
+	API_BASE_URL = API_URL + API_KEY
 
 	curl, err := exec.LookPath("curl")
 	if err != nil {
@@ -57,7 +89,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// fmt.Println(search)
 	out, err := search.Output()
 	if err != nil {
 		log.Fatal(err)
@@ -65,10 +96,19 @@ func main() {
 	// fmt.Println(string(out))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		_, err := w.Write(out)
+		var data dictSearch
+
+		err := xml.Unmarshal(out, &data)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		_, err = w.Write([]byte(fmt.Sprint(data)))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(fmt.Sprint(data))
 	})
 	log.Fatal(http.ListenAndServe(":3000", nil))
 
