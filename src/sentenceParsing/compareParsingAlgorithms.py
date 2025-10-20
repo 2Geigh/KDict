29ab7617bc734ccedd356f0578d47f118dc6e66c
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-19 18:26:57 -0600

    revamped compareParsingAlgorithms.py
    Totally didn't use AI lmao

--- src/sentenceParsing/compareParsingAlgorithms.py
+++ src/sentenceParsing/compareParsingAlgorithms.py
@@ -1,39 +1,54 @@
-# import konlpy
 import timeit
-from konlpy.tag import Hannanum
-from konlpy.tag import Kkma
-from konlpy.tag import Komoran
+from konlpy.tag import Hannanum, Kkma, Komoran, Mecab, Okt
 from konlpy.utils import pprint
-from konlpy.tag import Mecab
 import openkorpos_dic
 
+# Initialize taggers
 kkma = Kkma()
 hannanum = Hannanum()
 komoran = Komoran()
 mecab = Mecab(dicpath=openkorpos_dic.DICDIR)
+okt = Okt()
 
-# Timing with setup to import necessary variables
-kkma_time = timeit.timeit("pprint(kkma.pos(u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고,'))", 
-                           setup="from __main__ import pprint, kkma", 
-                           number=10)
+# Function to time the POS tagging
+def test_pos_tagging(phrase: str, replicate: int):
+    # Use timeit to measure execution time of each tagger
+    kkma_time = timeit.timeit(f"pprint(kkma.pos('{phrase}'))", 
+                               setup="from __main__ import pprint, kkma", 
+                               number=replicate)
 
-print("\n")
-hannanum_time = timeit.timeit("pprint(hannanum.pos(u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고,'))", 
-                               setup="from __main__ import pprint, hannanum", 
-                               number=10)
+    hannanum_time = timeit.timeit(f"pprint(hannanum.pos('{phrase}'))", 
+                                   setup="from __main__ import pprint, hannanum", 
+                                   number=replicate)
 
-print("\n")
-komoran_time = timeit.timeit("pprint(komoran.pos(u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고,'))", 
-                               setup="from __main__ import pprint, komoran", 
-                               number=10)
+    komoran_time = timeit.timeit(f"pprint(komoran.pos('{phrase}'))", 
+                                  setup="from __main__ import pprint, komoran", 
+                                  number=replicate)
 
-print("\n")
-mecab_time = timeit.timeit("pprint(mecab.pos(u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고,'))", 
-                               setup="from __main__ import pprint, mecab, openkorpos_dic", 
-                               number=10)
+    mecab_time = timeit.timeit(f"pprint(mecab.pos('{phrase}'))", 
+                                setup="from __main__ import pprint, mecab, openkorpos_dic", 
+                                number=replicate)
 
-# Print timing results
-print(f'Kkma time: {kkma_time}')
-print(f'Hannanum time: {hannanum_time}')
-print(f'Komoran time: {komoran_time}')
-print(f'Mecab time: {mecab_time}')
+    okt_time = timeit.timeit(f"pprint(okt.pos('{phrase}'))", 
+                              setup="from __main__ import pprint, okt", 
+                              number=replicate)
+
+    # Print timing results
+    print(f'Phrase: {phrase}')
+    print(f'Kkma time: {kkma_time}')
+    print(f'Hannanum time: {hannanum_time}')
+    print(f'Komoran time: {komoran_time}')
+    print(f'Mecab time: {mecab_time}')
+    print(f'OkT time: {okt_time}')
+    print("\n")
+
+# Test phrases
+test_phrases = [
+    # u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고.',
+    u'대한민국의 언어는 아름다운 언어입니다.',
+    # u'자연은 언제나 우리에게 많은 교훈을 줍니다.'
+]
+
+# Test each phrase
+for phrase in test_phrases:
+    test_pos_tagging(phrase, 1)
