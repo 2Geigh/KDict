7d27f55b36116860dc2dccee59eafaf51d5ae86d
Nick Garcia <nicholasgarcia2003@protonmail.com>
2025-10-19 19:15:00 -0600

    added emoji handling to search

--- src/sentenceParsing/compareParsingAlgorithms.py
+++ src/sentenceParsing/compareParsingAlgorithms.py
@@ -13,25 +13,35 @@ okt = Okt()
 # Function to time the POS tagging
 def test_pos_tagging(phrase: str, replicate: int):
     # Use timeit to measure execution time of each tagger
-    kkma_time = timeit.timeit(f"pprint(kkma.pos('{phrase}'))", 
-                               setup="from __main__ import pprint, kkma", 
-                               number=replicate)
+    kkma_time = timeit.timeit(
+        f"pprint(kkma.pos('''{phrase}'''))",  # Use triple quotes for the phrase
+        setup="from __main__ import pprint, kkma", 
+        number=replicate
+    )
 
-    hannanum_time = timeit.timeit(f"pprint(hannanum.pos('{phrase}'))", 
-                                   setup="from __main__ import pprint, hannanum", 
-                                   number=replicate)
+    hannanum_time = timeit.timeit(
+        f"pprint(hannanum.pos('''{phrase}'''))",
+        setup="from __main__ import pprint, hannanum", 
+        number=replicate
+    )
 
-    komoran_time = timeit.timeit(f"pprint(komoran.pos('{phrase}'))", 
-                                  setup="from __main__ import pprint, komoran", 
-                                  number=replicate)
+    komoran_time = timeit.timeit(
+        f"pprint(komoran.pos('''{phrase}'''))", 
+        setup="from __main__ import pprint, komoran", 
+        number=replicate
+    )
 
-    mecab_time = timeit.timeit(f"pprint(mecab.pos('{phrase}'))", 
-                                setup="from __main__ import pprint, mecab, openkorpos_dic", 
-                                number=replicate)
+    mecab_time = timeit.timeit(
+        f"pprint(mecab.pos('''{phrase}'''))", 
+        setup="from __main__ import pprint, mecab, openkorpos_dic", 
+        number=replicate
+    )
 
-    okt_time = timeit.timeit(f"pprint(okt.pos('{phrase}'))", 
-                              setup="from __main__ import pprint, okt", 
-                              number=replicate)
+    okt_time = timeit.timeit(
+        f"pprint(okt.pos('''{phrase}'''))", 
+        setup="from __main__ import pprint, okt", 
+        number=replicate
+    )
 
     # Print timing results
     print(f'Phrase: {phrase}')
@@ -39,16 +49,20 @@ def test_pos_tagging(phrase: str, replicate: int):
     print(f'Hannanum time: {hannanum_time}')
     print(f'Komoran time: {komoran_time}')
     print(f'Mecab time: {mecab_time}')
-    print(f'OkT time: {okt_time}')
-    print("\n")
+    print(f'OkT time: {okt_time}\n')
 
 # Test phrases
-test_phrases = [
-    # u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권, 인간의 존엄과 가치, 그리고 남녀의 동등한 권리에 대한 신념을 재확인하였으며, 보다 폭넓은 자유속에서 사회적 진보와 보다 나은 생활수준을 증진하기로 다짐하였고.',
-    u'대한민국의 언어는 아름다운 언어입니다.',
-    # u'자연은 언제나 우리에게 많은 교훈을 줍니다.'
-]
+test_phrases = {
+    "long sentence": u'국제연합의 모든 사람들은 그 헌장에서 기본적 인권입니다.',
+    "english letters": u'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    "numbers and symbols": u'0123456789!@#$%^&*()_+-=[]{}|;:\'",.<>?',
+    "greek letters": u'αβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ',
+    "cyrillic letters": u'йи́йло́гика́бро́шьЛе́вДави́д',
+    "chinese characters": u'我好你好吗',
+    "hebrew characters": u'שלום, מה שלומך?',
+    "emoji and symbols": u'😊💡🚀✨🔍✈️🌍'
+}
 
 # Test each phrase
-for phrase in test_phrases:
-    test_pos_tagging(phrase, 1)
+for key in test_phrases:
+    test_pos_tagging(test_phrases[key], 1)
