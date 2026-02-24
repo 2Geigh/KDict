c3966f2b415cc669f76e78f1f0ea659261facb4c
Nick Garcia <nicholasgarcia2003@protonmail.com>
2026-02-23 22:51:48 -0700

    Added navbar

---
+++ desktop/frontend/src/pages/Collection/Collection.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react"
+
+const Collection: FC = () => {
+	return <>Collection</>
+}
+
+export default Collection
--- desktop/frontend/src/pages/Home/Home.tsx
+++ desktop/frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,12 @@
+import { Link } from "react-router-dom"
 import MainInput from "../../components/MainInput/MainInput"
 import "./Home.scss"
 
 const Home = () => {
 	return (
-		<div id="Home">
+		<div id="Home" className="page">
 			<div id="container">
 				<MainInput />
-				<a id="viewCollection" href="">
-					View collection
-				</a>
 			</div>
 		</div>
 	)
